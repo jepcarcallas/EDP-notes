da40ee321e0233f91af39ab3525ac9a67352ea40
Jhon Elmer Pantilgan Carcallas <jepcarcallas@addunet.ph>
2026-02-02 18:52:03 +0800

    edp notes

---
+++ index.js
@@ -0,0 +1,44 @@
+/*let x = 10;
+let y = 5;
+console.log("Sum:", x + y);
+console.log("Difference:", x - y);
+console.log("Product:", x * y);
+console.log("Quotient:", x / y); 
+*/
+
+/*let x = true
+let y = "Hello world"
+console.log(x, y) */
+/*
+let x = [1, 2, 3, 4, 5]
+console.log(x[0]) //access 1
+console.log(x[1]) //access 2
+console.log(x[2]) //access 3
+console.log(x[3]) //access 4
+console.log(x[4]) //access 5 
+
+console.log(x.length) //length of array*/
+
+/*let x = [1, 4, [2, 3], true, false, "hello", "world"]
+for (let i = 0; i < x.length; i++) {
+    console.log(x[i])
+}*/
+
+/*let x = [12, 2+3, [true,5], 0, 1, "Hello", "World"]
+let y  = (x[5])
+console.log(x[2][0]) //access true in nested array
+console.log(x[2][1]) //access 5 in nested array
+console.log(y)*/
+
+/*let x = [12, 2+3, [true,5], 0, 1, "Hello", "World"]
+for (let i = 0; i < x.length; i++) {
+    if (typeof x[i] === "number") {
+        console.log(x[i])
+    }
+}*/
+
+let x = []
+x[0] = 100
+console.log
+x[7] = 544
+console.log(x)
